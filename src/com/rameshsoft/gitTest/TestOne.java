e58523323141db1c82950c9178db493cf372ed31
associateuser1 <aasociateuser1@gmail.com>
2023-11-30 14:22:00 +0530

    updated test one

--- src/com/rameshsoft/gitTest/TestOne.java
+++ src/com/rameshsoft/gitTest/TestOne.java
@@ -3,7 +3,7 @@ package com.rameshsoft.gitTest;
 public class TestOne {
 	public static void main(String[] args) {
 		System.out.println("*****TEST ONE******");
+		System.out.println("*****UpdatedTEST ONE******");
 		
 	}
-
 }
